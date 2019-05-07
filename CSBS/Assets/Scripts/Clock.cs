053a543b181a893e4d2020339524f049e66a91ea
Janice <janiceng@ufl.edu>
2019-05-07 17:11:12 -0400

    DontDestroy object

--- CSBS/Assets/Scripts/Clock.cs
+++ CSBS/Assets/Scripts/Clock.cs
@@ -6,20 +6,11 @@ using UnityEngine.UI;
 public class Clock : MonoBehaviour
 {
     public Text text;
-    public int hour = 0;
-    public int ticker = 0;
-
-    // Update is called once per frame
+    public int CurrHour;
+    
     void Update()
     {
-        ticker++;
-        if (ticker >= 100) {
-            ticker = 0;
-            hour++;
-            text.text = "Day " + hour;
-        }
-        if (hour >= 24) {
-            hour = 0;
-        }
+        CurrHour = GameData.hour;
+        text.text = "Day " + CurrHour;
     }
 }
---
+++ CSBS/Assets/Scripts/GameData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameData : MonoBehaviour
+{
+    // Clock Data
+    public static int hour = 0;
+    public static int ticker = 0;
+    public int test;
+
+    // Other Data
+
+
+    void Update() {
+        ticker++;
+        if (ticker >= 100) {
+            ticker = 0;
+            hour++;
+        }
+        if (hour >= 24) {
+            hour = 0;
+        }
+        test = hour;
+    }
+
+    void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
