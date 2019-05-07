ce53a76de8d57e7a2e71318eab80adce19246a07
Janice <janiceng@ufl.edu>
2019-05-07 11:34:49 -0400

    Doors and Scenes

---
+++ CSBS/Assets/Scripts/Clock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    public Text text;
+    public int hour = 0;
+    public int ticker = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ticker++;
+        if (ticker >= 100) {
+            ticker = 0;
+            hour++;
+            text.text = "Day " + hour;
+        }
+        if (hour >= 24) {
+            hour = 0;
+        }
+    }
+}
---
+++ CSBS/Assets/Scripts/Portal.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public int DoorNumber = 0;
+    private int SelectedDoor = 0;
+    private bool IsEntering = false;
+
+    void Start() {
+        if (DoorNumber == 0) {
+            Debug.Log("Need to assign door number.");
+        }
+    }
+
+    void OnMouseEnter() {
+        Debug.Log("Enter");
+        // Change animation
+        // TODO
+        // Set SelectedDoor
+        SelectedDoor = DoorNumber;
+    }
+
+    void OnMouseExit() {
+        // Reset SelectedDoor
+        SelectedDoor = 0;
+    }
+
+    void OnMouseDown() {
+        Debug.Log("Clicked on " + SelectedDoor);
+        IsEntering = true;
+    }
+
+    void Update() {
+        // Check if clicked
+        if (IsEntering) {
+            ChangeScene(SelectedDoor);
+        }
+    }
+
+    void ChangeScene(int num) {
+        switch(num) {
+            case 1:
+                SceneManager.LoadScene("Scenes/Rooms/VTA");
+            break;
+            case 2:
+                SceneManager.LoadScene("Scenes/Rooms/Placeholder");
+            break;
+            case 3:
+                SceneManager.LoadScene("Scenes/Rooms/Stomach");
+            break;
+            case 4:
+                SceneManager.LoadScene("Scenes/Rooms/Amygdala");
+            break;
+            case 5:
+                SceneManager.LoadScene("Scenes/GameScene");
+            break;
+        }
+    }
+
+}
