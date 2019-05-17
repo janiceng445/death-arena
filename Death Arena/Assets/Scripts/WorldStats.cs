645a831be3cd88700b9501769d00ca414aae1a2b
janiceng445 <janiceng@ufl.edu>
2019-05-17 00:38:03 -0400

    Stats updated

--- Death Arena/Assets/Scripts/WorldStats.cs
+++ Death Arena/Assets/Scripts/WorldStats.cs
@@ -7,10 +7,16 @@ public class WorldStats : MonoBehaviour
     public static int level;
     public static int gold;
     public static bool paused;
+    public static float volume;
 
     void Start() {
         level = 1;
         gold = 1500000;
         paused = true;
+        AudioListener.volume = 0.5f;
+    }
+
+    void Update() {
+        //volume = GameObject.FindGameObjectWithTag("Volume").GetComponent<Slider>().value;
     }
 }
