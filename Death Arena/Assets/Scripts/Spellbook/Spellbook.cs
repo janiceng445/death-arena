2a7558e181dfea4ce35f9ffc73237b85306e8601
janiceng445 <janiceng@ufl.edu>
2019-06-06 10:07:41 -0400

    pages

--- Death Arena/Assets/Scripts/Spellbook/Spellbook.cs
+++ Death Arena/Assets/Scripts/Spellbook/Spellbook.cs
@@ -1,34 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spellbook : MonoBehaviour
 {
     public static int page;
     private int numPages;
+    private GameObject prevPage;
     private GameObject currPage;
     private Animator animator;
+    private GameObject[] canvases;
+    private List<GameObject> sorting;
 
     void Start() {
         page = 0;
         numPages = 6;
-        animator = GameObject.Find("Page_0" + page.ToString()).GetComponent<Animator>();
-        for (int i = 1; i < numPages; i++) {
-            //GameObject.Find("Page_0" + i.ToString()).GetComponent<Animator>().enabled = false;
+        prevPage = null;
+        sorting = new List<GameObject>();
+        currPage = GameObject.Find("pg" + page.ToString());
+        animator = GameObject.Find("pg" + page.ToString()).GetComponent<Animator>();
+        canvases = new GameObject[numPages];
+        for (int i = 0; i < numPages; i++) {
+            canvases[i] = GameObject.Find("cv" + i);
         }
     }
 
     public void NextPage() {
         if (page != numPages - 1) {
             animator.Play("PageForward");
+            
+            if (prevPage != null) {
+                Debug.Log(prevPage);
+                sorting.Add(prevPage);
+                ReSort();
+            }
+            else {
+                currPage.transform.parent.GetComponent<Canvas>().sortingOrder--;
+            }
             page++;
-            //animator.enabled = false;
+            
+            prevPage = currPage;
             FindPage();
+            
         }
     }
 
     public void PreviousPage() {
         if (page != 0) {
+            prevPage = currPage;
+            if (prevPage != null) prevPage.transform.parent.GetComponent<Canvas>().sortingOrder++;
             page--;
             FindPage();
             animator.Play("PageBack");
@@ -37,8 +58,36 @@ public class Spellbook : MonoBehaviour
 
     void FindPage() {
         Debug.Log(page);
-        animator = GameObject.Find("Page_0" + page.ToString()).GetComponent<Animator>();
-        //animator.enabled = true;
-        currPage = GameObject.Find("Page_0" + page.ToString());
+        animator = GameObject.Find("pg" + page.ToString()).GetComponent<Animator>();
+        currPage = GameObject.Find("pg" + page.ToString());
+    }
+
+    void FixSortingLayers(bool direction) {
+        // If true, increment sorting layers
+        if (direction) {
+            for (int i = 0; i < numPages; i++) {
+                canvases[i].GetComponent<Canvas>().sortingOrder++;
+                if (canvases[i].GetComponent<Canvas>().sortingOrder > numPages - 1) {
+                    canvases[i].GetComponent<Canvas>().sortingOrder = 0;
+                }
+            }
+        }
+        // If false, decrement sorting layers
+        else {
+            for (int i = 0; i < numPages; i++) {
+                canvases[i].GetComponent<Canvas>().sortingOrder--;
+                if (canvases[i].GetComponent<Canvas>().sortingOrder < 0) {
+                    canvases[i].GetComponent<Canvas>().sortingOrder = numPages - 1;
+                }
+            }
+        }
+    }
+
+    void ReSort() {
+        Debug.Log("resorting");
+        Debug.Log(sorting.Count);
+        foreach (GameObject g in sorting) {
+            g.transform.parent.GetComponent<Canvas>().sortingOrder--;
+        }
     }
 }
